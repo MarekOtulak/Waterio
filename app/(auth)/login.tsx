2cff032561bd34289769b08deadd89e95d674766
MarekOtulak <omarco2109@gmail.com>
2025-05-18 15:59:00 +0200

    Small fixes.

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -46,7 +46,7 @@ export default function Login() {
     };
 
     const navigateToRegister = () => {
-        router.push('/(auth)/register');
+        router.replace('/(auth)/register');
     };
 
     return (
